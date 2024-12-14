03ff3683ddc8936c07c139166a192f16e7630997
Sarah Moorby <lhijd8vq@students.codeinstitute.net>
2024-12-14 15:05:18 +0000

    jdksljdklsajdfsk#

--- corporate_calendar/settings.py
+++ corporate_calendar/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'corporate_calendar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
